1852f17c236cf813e9f881f4d7a081db0c8726d8
Mayank <160305552+mayank0290@users.noreply.github.com>
2025-05-03 21:22:26 +0530

    Add files via upload

---
+++ pr01/tweet/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['text', 'photo']
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+
+    def clean_password2(self):
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password and password2 and password != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2        
